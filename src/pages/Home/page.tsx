9e5f033aa50a91ea8099de2a09c734d2a2f796fd
DMAN29 <ranjan.dr29@gmail.com>
2025-09-12 12:26:46 +0530

    created project highlights

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,17 +1,19 @@
 import Banner from "@/components/Banner/page";
 import Gallery from "@/components/Gallery/page";
 import OurService from "@/components/OurService/page";
+import ProjectHighlights from "@/components/ProjectHighlights/page";
 import Team from "@/components/Team/page";
 import WhoWeAre from "@/components/WeAre/page";
 
 export default function HomePage() {
   return (
     <div className="">
-      <Banner />
-      <WhoWeAre />
-      <OurService />
-      <Team />
-      <Gallery />
+      {/* <Banner /> */}
+      {/* <WhoWeAre /> */}
+      {/* <OurService /> */}
+      <ProjectHighlights />
+      {/* <Team /> */}
+      {/* <Gallery /> */}
     </div>
   );
 }
