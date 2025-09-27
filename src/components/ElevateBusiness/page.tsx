db9bbd13a4b09d13adc32329bf172457833bf4da
DMAN29 <ranjan.dr29@gmail.com>
2025-09-27 17:24:59 +0530

    ElevateBusinessPage fixed

--- src/components/ElevateBusiness/page.tsx
+++ src/components/ElevateBusiness/page.tsx
@@ -1,25 +1,39 @@
 import Link from "next/link";
 
+interface Button {
+  label: string;
+  url: string;
+}
+
+interface ElevateBusinessPageData {
+  title: string;
+  buttons: Button[];
+}
+
 interface ElevateBusinessPageProps {
-  data: {
-    title: string;
-    buttons: {
-      label: string;
-      url: string;
-    }[];
-  };
+  data?: ElevateBusinessPageData;
 }
 
 export default function ElevateBusinessPage({
   data,
 }: ElevateBusinessPageProps) {
+  const finalData = data ?? {
+    title: "Elevate your business with professional Development services",
+    buttons: [
+      {
+        label: "Connect with our Experts Today",
+        url: "/contact-us",
+      },
+    ],
+  };
+
   return (
     <div className="bg-[#003049] mb-10">
       <div className="max-w-7xl mx-auto px-4 py-15 text-center">
         <h1 className="text-white md:text-2xl font-medium mb-4">
-          {data.title}
+          {finalData.title}
         </h1>
-        {data.buttons.map((button, index) => (
+        {finalData.buttons.map((button, index) => (
           <Link href={button.url} key={index}>
             <button
               className="bg-white text-[#363636] px-3 py-2 cursor-pointer rounded-full font-medium text-xs md:text-base"
